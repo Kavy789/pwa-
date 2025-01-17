adb77f9f83b8880af13f5d7a842bd465aa2cb8fd
Kavy789 <131178980+Kavy789@users.noreply.github.com>
2025-01-17 21:13:57 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,46 +1,8 @@
-const CACHE_NAME = 'my-pwa-cache-v1';
-const urlsToCache = [
-    '/',
-    '/styles.css',
-    '/app.js',
-    '/index.html',
-    '/manifest.json',
-    '/icon-192x192.png',
-    '/icon-512x512.png'
-];
-
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => {
-                return cache.addAll(urlsToCache);
-            })
-    );
-});
-
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-            .then((response) => {
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request);
-            })
-    );
-});
-
-self.addEventListener('activate', (event) => {
-    const cacheWhitelist = [CACHE_NAME];
-    event.waitUntil(
-        caches.keys().then((keyList) => {
-            return Promise.all(
-                keyList.map((key) => {
-                    if (cacheWhitelist.indexOf(key) === -1) {
-                        return caches.delete(key);
-                    }
-                })
-            );
-        })
-    );
-});
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js')
+        .then((registration) => {
+            console.log('Service Worker registered with scope:', registration.scope);
+        }).catch((error) => {
+            console.log('Service Worker registration failed:', error);
+        });
+}
